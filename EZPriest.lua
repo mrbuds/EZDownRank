bb3f3420cacd723eb8e180f76a60f2e1efaaba49
Buds <mrbouyou@gmail.com>
2020-05-31 03:54:01 +0200

    add druid support use talent bonus

--- EZPriest.lua
+++ EZPriest.lua
@@ -1,4 +1,5 @@
-local GetSpellBonusHealing, UnitPower, UnitHealthMax, UnitHealth, CreateFrame, C_Timer, InCombatLockdown, GetTime = GetSpellBonusHealing, UnitPower, UnitHealthMax, UnitHealth, CreateFrame, C_Timer, InCombatLockdown, GetTime
+local GetSpellBonusHealing, UnitPower, UnitHealthMax, UnitHealth, CreateFrame, C_Timer, InCombatLockdown = GetSpellBonusHealing, UnitPower, UnitHealthMax, UnitHealth, CreateFrame, C_Timer, InCombatLockdown
+local IsInRaid, GetNumSubgroupMembers, GetNumGroupMembers, GetTime, GetTalentInfo = IsInRaid, GetNumSubgroupMembers, GetNumGroupMembers, GetTime, GetTalentInfo
 local LGF = LibStub("LibGetFrame-1.0")
 local GetUnitFrame = LGF.GetUnitFrame
 local debug = false
@@ -12,50 +13,120 @@ end
 local spells = {
     PRIEST = {
         normal = {
-            { name = "F1", cost = 125, spellId = 2061, baseCastTime = 1.5 },
-            { name = "F2", cost = 155, spellId = 9472, baseCastTime = 1.5 },
-            { name = "F3", cost = 185, spellId = 9473, baseCastTime = 1.5 },
-            { name = "F4", cost = 215, spellId = 9474, baseCastTime = 1.5 },
-            { name = "F5", cost = 265, spellId = 10915, baseCastTime = 1.5 },
-            { name = "F6", cost = 315, spellId = 10916, baseCastTime = 1.5 },
-            { name = "F7", cost = 380, spellId = 10917, baseCastTime = 1.5 },
+            ranks = {
+                { name = "F1", cost = 125, spellId = 2061, baseCastTime = 1.5 },
+                { name = "F2", cost = 155, spellId = 9472, baseCastTime = 1.5 },
+                { name = "F3", cost = 185, spellId = 9473, baseCastTime = 1.5 },
+                { name = "F4", cost = 215, spellId = 9474, baseCastTime = 1.5 },
+                { name = "F5", cost = 265, spellId = 10915, baseCastTime = 1.5 },
+                { name = "F6", cost = 315, spellId = 10916, baseCastTime = 1.5 },
+                { name = "F7", cost = 380, spellId = 10917, baseCastTime = 1.5 },
+            },
+            bonusFn = function()
+                local _, _, _, _, rank  = GetTalentInfo(2, 15)
+                return 1 + (rank * 0.02)
+            end,
         },
         shift = {
-            { name = "H1", cost = 132, spellId = 2054, baseCastTime = 3 },
-            { name = "H2", cost = 174, spellId = 2055, baseCastTime = 3 },
-            { name = "H3", cost = 217, spellId = 6063, baseCastTime = 3 },
-            { name = "H4", cost = 259, spellId = 6064, baseCastTime = 3 },
-            { name = "GH1", cost = 314, spellId = 2060, baseCastTime = 3 },
-            { name = "GH2", cost = 387, spellId = 10963, baseCastTime = 3 },
-            { name = "GH3", cost = 463, spellId = 10964, baseCastTime = 3 },
-            { name = "GH4", cost = 557, spellId = 10965, baseCastTime = 3 },
+            ranks = {
+                { name = "H1", cost = 132, spellId = 2054, baseCastTime = 3 },
+                { name = "H2", cost = 174, spellId = 2055, baseCastTime = 3 },
+                { name = "H3", cost = 217, spellId = 6063, baseCastTime = 3 },
+                { name = "H4", cost = 259, spellId = 6064, baseCastTime = 3 },
+                { name = "GH1", cost = 314, spellId = 2060, baseCastTime = 3 },
+                { name = "GH2", cost = 387, spellId = 10963, baseCastTime = 3 },
+                { name = "GH3", cost = 463, spellId = 10964, baseCastTime = 3 },
+                { name = "GH4", cost = 557, spellId = 10965, baseCastTime = 3 },
+            },
+            bonusFn = function()
+                local _, _, _, _, rank  = GetTalentInfo(2, 15)
+                return 1 + (rank * 0.02)
+            end,
         },
         -- ctrl = {},
         -- alt = {},
     },
     SHAMAN = {
         normal = {
-            { name = "LHW1", cost = 105, spellId = 8004, baseCastTime = 1.5 },
-            { name = "LHW2", cost = 145, spellId = 8008, baseCastTime = 1.5 },
-            { name = "LHW3", cost = 185, spellId = 8010, baseCastTime = 1.5 },
-            { name = "LHW4", cost = 235, spellId = 10466, baseCastTime = 1.5 },
-            { name = "LHW5", cost = 305, spellId = 10467, baseCastTime = 1.5 },
-            { name = "LHW6", cost = 380, spellId = 10468, baseCastTime = 1.5 },
+            ranks = {
+                { name = "LHW1", cost = 105, spellId = 8004, baseCastTime = 1.5 },
+                { name = "LHW2", cost = 145, spellId = 8008, baseCastTime = 1.5 },
+                { name = "LHW3", cost = 185, spellId = 8010, baseCastTime = 1.5 },
+                { name = "LHW4", cost = 235, spellId = 10466, baseCastTime = 1.5 },
+                { name = "LHW5", cost = 305, spellId = 10467, baseCastTime = 1.5 },
+                { name = "LHW6", cost = 380, spellId = 10468, baseCastTime = 1.5 },
+            },
+            bonusFn = function()
+                local _, _, _, _, rank  = GetTalentInfo(3, 14)
+                return 1 + (rank * 0.02)
+            end,
         },
         shift = {
-            { name = "HW3", cost = 80, spellId = 547, baseCastTime = 2.5 },
-            { name = "HW4", cost = 155, spellId = 913, baseCastTime = 3 },
-            { name = "HW5", cost = 200, spellId = 939, baseCastTime = 3 },
-            { name = "HW6", cost = 265, spellId = 959, baseCastTime = 3 },
-            { name = "HW7", cost = 340, spellId = 8005, baseCastTime = 3 },
-            { name = "HW8", cost = 440, spellId = 10395, baseCastTime = 3 },
-            { name = "HW9", cost = 560, spellId = 10396, baseCastTime = 3 },
-            { name = "HW10", cost = 620, spellId = 25357, baseCastTime = 3 },
+            ranks = {
+                -- { name = "HW1", cost = 25, spellId = 331, baseCastTime = 1.5 },
+                -- { name = "HW2", cost = 45, spellId = 332, baseCastTime = 2 },
+                { name = "HW3", cost = 80, spellId = 547, baseCastTime = 2.5 },
+                { name = "HW4", cost = 155, spellId = 913, baseCastTime = 3 },
+                { name = "HW5", cost = 200, spellId = 939, baseCastTime = 3 },
+                { name = "HW6", cost = 265, spellId = 959, baseCastTime = 3 },
+                { name = "HW7", cost = 340, spellId = 8005, baseCastTime = 3 },
+                { name = "HW8", cost = 440, spellId = 10395, baseCastTime = 3 },
+                { name = "HW9", cost = 560, spellId = 10396, baseCastTime = 3 },
+                { name = "HW10", cost = 620, spellId = 25357, baseCastTime = 3 },
+            },
+            bonusFn = function()
+                local _, _, _, _, rank  = GetTalentInfo(3, 14)
+                return 1 + (rank * 0.02)
+            end,
         },
         ctrl = {
-            { name = "CH1", cost = 260, spellId = 1064, baseCastTime = 2.5 },
-            { name = "CH2", cost = 315, spellId = 10622, baseCastTime = 2.5 },
-            { name = "CH3", cost = 405, spellId = 10623, baseCastTime = 2.5 },
+            ranks = {
+                { name = "CH1", cost = 260, spellId = 1064, baseCastTime = 2.5 },
+                { name = "CH2", cost = 315, spellId = 10622, baseCastTime = 2.5 },
+                { name = "CH3", cost = 405, spellId = 10623, baseCastTime = 2.5 },
+            },
+            bonusFn = function()
+                local _, _, _, _, rank  = GetTalentInfo(3, 14)
+                return 1 + (rank * 0.02)
+            end,
+        }
+    },
+    DRUID = {
+        normal = {
+            ranks = {
+                -- { name = "HT1", cost = 25, spellId = 5185, baseCastTime = 1.5 },
+                -- { name = "HT2", cost = 55, spellId = 5186, baseCastTime = 2 },
+                -- { name = "HT3", cost = 110, spellId = 5187, baseCastTime = 2.5 },
+                { name = "HT4", cost = 185, spellId = 5188, baseCastTime = 3 },
+                { name = "HT5", cost = 270, spellId = 5189, baseCastTime = 3.5 },
+                { name = "HT6", cost = 335, spellId = 6778, baseCastTime = 3.5 },
+                { name = "HT7", cost = 405, spellId = 8903, baseCastTime = 3.5 },
+                { name = "HT8", cost = 495, spellId = 9758, baseCastTime = 3.5 },
+                { name = "HT9", cost = 600, spellId = 9888, baseCastTime = 3.5 },
+                { name = "HT10", cost = 720, spellId = 9889, baseCastTime = 2.5 },
+                { name = "HT11", cost = 800, spellId = 25297, baseCastTime = 3.5 },
+            },
+            bonusFn = function()
+                local _, _, _, _, rank  = GetTalentInfo(3, 12)
+                return 1 + (rank * 0.02)
+            end,
+        },
+        shift = {
+            ranks = {
+                -- { name = "RG1", cost = 120, spellId = 8936, baseCastTime = 2 },
+                { name = "RG2", cost = 205, spellId = 8938, baseCastTime = 2 },
+                { name = "RG3", cost = 280, spellId = 8939, baseCastTime = 2 },
+                { name = "RG4", cost = 350, spellId = 8940, baseCastTime = 2 },
+                { name = "RG5", cost = 420, spellId = 8941, baseCastTime = 2 },
+                { name = "RG6", cost = 510, spellId = 9750, baseCastTime = 2 },
+                { name = "RG7", cost = 615, spellId = 9856, baseCastTime = 2 },
+                { name = "RG8", cost = 740, spellId = 9857, baseCastTime = 2 },
+                { name = "RG9", cost = 880, spellId = 9858, baseCastTime = 2 },
+            },
+            bonusFn = function()
+                local _, _, _, _, rank  = GetTalentInfo(3, 12)
+                return 1 + (rank * 0.02)
+            end,
         }
     }
 }
@@ -64,6 +135,9 @@ local myClass = select(2, UnitClass("player"))
 if not spells[myClass] then return end
 
 local mySpells = spells[myClass]
+for _, v in pairs(mySpells) do
+    v.bonus = v.bonusFn()
+end
 
 local hiddenTooltip
 local function GetHiddenTooltip()
@@ -83,7 +157,7 @@ local function getMinMax(spellId)
     tooltip:ClearLines()
     tooltip:SetSpellByID(spellId)
     local tooltipTextLine = select(9, tooltip:GetRegions())
-    local tooltipText = tooltipTextLine and tooltipTextLine:GetObjectType() == "FontString" and tooltipTextLine:GetText() or "";
+    local tooltipText = tooltipTextLine and tooltipTextLine:GetObjectType() == "FontString" and tooltipTextLine:GetText() or ""
     return tooltipText:match("(%d+) .+ (%d+)")
 end
 
@@ -98,7 +172,7 @@ local maxCostTable = {}
 for _, key in ipairs({"normal", "shift", "ctrl", "alt"}) do
     maxCostTable[key] = 0
     if mySpells[key] then
-        for _, spell in pairs(mySpells[key]) do
+        for _, spell in pairs(mySpells[key].ranks) do
             if spell.cost > maxCostTable[key] then
                 maxCostTable[key] = spell.cost
             end
@@ -167,14 +241,17 @@ local updateUnitColor = function(unit)
     for i = 8, 1, -1 do
         local button = buttons[unit.."-"..i]
         if button then
-            local spell = activeSpells[i]
+            local spell = activeSpells.ranks[i]
             if spell then
                 if not spell.max then
                     spell.min, spell.max = getMinMax(spell.spellId)
                 end
+                if spell.bonus == 0 and spell.bonusFn then
+                    spell.bonus = spell.bonusFn()
+                end
                 if spell.max then
                     local bonus = healingPower * (spell.baseCastTime / 3.5)
-                    local spellMaxHealing = spell.max + bonus -- calculate max heal
+                    local spellMaxHealing = (spell.max + bonus) * activeSpells.bonus -- calculate max heal
                     if spellMaxHealing > deficit then
                         button.texture:SetColorTexture(1, 0, 0, 0) -- invisible
                     else
@@ -241,18 +318,18 @@ local InitSquares = function()
                 end
                 button:SetAttribute("unit", unit)
                 button:SetAttribute("type1", "spell")
-                button:SetAttribute("spell1", mySpells.normal[i] and mySpells.normal[i].spellId)
-                if mySpells.shift and mySpells.shift[i] and mySpells.shift[i].spellId then
+                button:SetAttribute("spell1", mySpells.normal.ranks[i] and mySpells.normal.ranks[i].spellId)
+                if mySpells.shift and mySpells.shift.ranks[i] and mySpells.shift.ranks[i].spellId then
                     button:SetAttribute("shift-type1", "spell")
-                    button:SetAttribute("shift-spell1", mySpells.shift[i] and mySpells.shift[i].spellId)
+                    button:SetAttribute("shift-spell1", mySpells.shift.ranks[i].spellId)
                 end
-                if mySpells.ctrl and mySpells.ctrl[i] and mySpells.ctrl[i].spellId then
+                if mySpells.ctrl and mySpells.ctrl.ranks[i] and mySpells.ctrl.ranks[i].spellId then
                     button:SetAttribute("ctrl-type1", "spell")
-                    button:SetAttribute("ctrl-spell1", mySpells.ctrl[i].spellId)
+                    button:SetAttribute("ctrl-spell1", mySpells.ctrl.ranks[i].spellId)
                 end
-                if mySpells.alt and mySpells.alt[i] and mySpells.alt[i].spellId then
+                if mySpells.alt and mySpells.alt.ranks[i] and mySpells.alt.ranks[i].spellId then
                     button:SetAttribute("alt-type1", "spell")
-                    button:SetAttribute("alt-spell1", mySpells.alt[i].spellId)
+                    button:SetAttribute("alt-spell1", mySpells.alt.ranks[i].spellId)
                 end
                 button:SetSize(ssize, ssize)
                 button.texture:SetColorTexture(1, 0, 0, 0)
